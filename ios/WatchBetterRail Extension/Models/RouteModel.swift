fa8a2da92255417784df2a476f51672d5ba4471e
Guy Tepper <hey@guytepper.com>
2023-04-10 10:47:56 +0300

    v1.11.0

--- ios/WatchBetterRail Extension/Models/RouteModel.swift
+++ ios/WatchBetterRail Extension/Models/RouteModel.swift
@@ -66,7 +66,7 @@ struct RouteModel {
   func fetchRoute(originId: Int, destinationId: Int, date: Date? = nil, completion: @escaping ([Route]) -> Void) {
       let (routeDate, routeTime) = formatRouteDate(date ?? Date())
       
-      let url = URL(string: "https://israelrail.azurefd.net/rjpa-prod/api/v1/timetable/searchTrainLuzForDateTime?fromStation=\(originId)&toStation=\(destinationId)&date=\(routeDate)&hour=\(routeTime)&scheduleType=1&systemType=2&languageId=Hebrew")!
+      let url = URL(string: "https://israelrail.azurefd.net/rjpa-prod/api/v1/timetable/searchTrainLuzForDateTime?fromStation=\(originId)&toStation=\(destinationId)&date=\(routeDate)&hour=\(routeTime)&scheduleType=1&systemType=1&languageId=Hebrew")!
       
       var request = URLRequest(url: url)
       request.addValue("4b0d355121fe4e0bb3d86e902efe9f20", forHTTPHeaderField: "Ocp-Apim-Subscription-Key")
